682e8a09a1affd24ed6e57e2f2f33a8e25ad06e5
Thiago Minguim <chazy249@gmail.com>
2026-05-18 03:00:02 -0300

    Revert "Padroniza mensagem dos links de WhatsApp"
    This reverts commit 0af27ce2ea3d875b46dbf62f4a65d694854c0c4a.

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -1,16 +1,24 @@
 export const WHATSAPP_PHONE = '553484262358'
-const DEFAULT_WHATSAPP_MESSAGE = 'Olá, Mariana! Vim pelo site.'
 
 export const whatsappMessages = {
-  google: DEFAULT_WHATSAPP_MESSAGE,
-  site: DEFAULT_WHATSAPP_MESSAGE,
-  vistoria: DEFAULT_WHATSAPP_MESSAGE,
-  laudo: DEFAULT_WHATSAPP_MESSAGE,
-  inspecao: DEFAULT_WHATSAPP_MESSAGE,
-  assistencia: DEFAULT_WHATSAPP_MESSAGE,
-  pericia: DEFAULT_WHATSAPP_MESSAGE,
-  manutencao: DEFAULT_WHATSAPP_MESSAGE,
-  duvida: DEFAULT_WHATSAPP_MESSAGE,
+  google:
+    'Olá, Mariana! Vim pelo Google e gostaria de orientação sobre um serviço de engenharia diagnóstica.',
+  site:
+    'Olá, Mariana! Vim pelo site e gostaria de orientação sobre um serviço de engenharia diagnóstica.',
+  vistoria:
+    'Olá, Mariana! Vim pelo site e gostaria de saber mais sobre vistoria técnica.',
+  laudo:
+    'Olá, Mariana! Vim pelo site e preciso de orientação sobre laudo técnico.',
+  inspecao:
+    'Olá, Mariana! Vim pelo site e gostaria de informações sobre inspeção predial.',
+  assistencia:
+    'Olá, Mariana! Vim pelo site e gostaria de orientação sobre assistência técnica.',
+  pericia:
+    'Olá, Mariana! Vim pelo site e gostaria de informações sobre perícia em ações judiciais.',
+  manutencao:
+    'Olá, Mariana! Vim pelo site e gostaria de informações sobre plano de manutenção predial.',
+  duvida:
+    'Olá, Mariana! Vim pelo site e gostaria de tirar uma dúvida sobre qual serviço preciso.',
 }
 
 export function getWhatsAppUrl(message = whatsappMessages.site) {
